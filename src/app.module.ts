8cc4a16ee3791a1071bdfce02a673c723b931ecc
IditAvrahami <iditavra@gmail.com>
2023-12-12 21:16:27 +0200

    connect to docker and pgadmin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'db',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+    entities: [],
+    synchronize: true,
+    autoLoadEntities: true,
+  }),AuthModule, UserModule],
 })
 export class AppModule {}
